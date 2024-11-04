92a2d5f90b9bdd2ae488c7e6f37770c3a4294834
Anastasiya-Khokhlova <khokhlova.st@gmail.com>
2024-11-04 19:41:45 +0400

    Lesson_14_testng

---
+++ src/test/java/org/example/FactorialTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+public class FactorialTest {
+
+    @Test
+    public void calculatePositiveTest() {
+        String actualResult = Factorial.calculate(4);
+        String expectedResult = "24";
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void calculateIllegalInsertTest() {
+        String actualResult = Factorial.calculate(-1);
+        String expectedResult = "ошибка";
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void calculateNegativeTest() {
+        String actualResult = Factorial.calculate(4);
+        String expectedResult = "25";
+        Assert.assertNotEquals(expectedResult, actualResult);
+    }
+
+}
